5aa48e9dd9c0e5d7dbac4fff9b55eb96a698f194
eche-unicorn <d.anasteysha@gmail.com>
2022-02-22 21:28:40 +0200

    hw05 completed

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const usersRouter = require('./routes/api/users');
 
 const app = express();
 
-// const upload = multer({ dest: })
-
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
@@ -22,7 +20,6 @@ app.use(express.static('public'));
 
 app.use('/api/contacts', contactsRouter);
 app.use('/api/users', usersRouter);
-// app.post('/api/contacts', async (req, res, next) => {});
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
--- controllers/users/patchAvatar.js
+++ controllers/users/patchAvatar.js
@@ -6,38 +6,22 @@ const Jimp = require('jimp');
 
 const patchAvatar = async (req, res, next) => {
   const { path: tmpUpload, filename } = req.file;
+
   const { _id } = req.user;
   try {
+    await Jimp.read(tmpUpload)
+      .then(avatar => {
+        return avatar.resize(250, 250).write(tmpUpload);
+      })
+      .catch(err => console.error(err));
+
     const [extension] = filename.split('.').reverse();
-    console.log(filename);
     const newFilename = `${_id}.${extension}`;
     const resultingPath = path.join(avatarsDir, newFilename);
-
     await fs.rename(tmpUpload, resultingPath);
 
-    //   jimp
-
-    // open a file called "lenna.png"
-    // Jimp.read('lenna.png', (err, lenna) => {
-    //   if (err) throw err;
-    //   lenna
-    //     .resize(256, 256) // resize
-    //     .quality(60) // set JPEG quality
-    //     .greyscale() // set greyscale
-    //     .write('lena-small-bw.jpg'); // save
-    // });
-
-    //   Jimp.read(newFilename, (err, avatar) => {
-    //     if (err) {
-    //       throw err;
-    //     }
-    //     const [extension, ...rest] = newFilename.split('.').reverse();
-    //     const resizedAvatarName = `${rest.reverse()}-small.${extension}`;
-    //     avatar.resize(250, 250, Jimp.RESIZE_BILINEAR).write(resizedAvatarName);
-    //     console.log(resizedAvatarName);
-    //   });
-
     const avatarURL = path.join('avatars', newFilename);
+
     await User.findByIdAndUpdate(_id, { avatarURL });
     res.json(`avatarURL: ${avatarURL}`);
   } catch (error) {
